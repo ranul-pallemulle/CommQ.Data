a133a9ccea25a115cd7e1753000df501976bc8ab
Ranul Pallemulle <ranulp96@gmail.com>
2024-03-30 09:40:55 +0400

    Move OpenAndGet to connection factory interface

--- CommQ.Data/IConnectionFactory.cs
+++ CommQ.Data/IConnectionFactory.cs
@@ -1,9 +1,25 @@
 ﻿using System.Data;
+using System.Data.Common;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace CommQ.Data
 {
     public interface IConnectionFactory
     {
         IDbConnection Create();
+        async Task<IDbConnection> OpenAndGetAsync(CancellationToken cancellationToken = default)
+        {
+            var connection = Create();
+            if (connection is DbConnection conn)
+            {
+                await conn.OpenAsync(cancellationToken).ConfigureAwait(false);
+            }
+            else
+            {
+                connection.Open();
+            }
+            return connection;
+        }
     }
 }
