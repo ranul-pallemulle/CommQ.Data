940cf96995e0f4d7c67fb0ec58f64047918e2f9a
Ranul Pallemulle <ranulp96@gmail.com>
2024-04-01 17:51:45 +0400

    Add SqlConnectionFactory

---
+++ CommQ.Data.SqlServer/SqlConnectionFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace CommQ.Data.SqlServer
+{
+    public class SqlConnectionFactory : IConnectionFactory
+    {
+        private readonly string _connectionString;
+
+        public SqlConnectionFactory(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public IDbConnection Create()
+        {
+            return new SqlConnection(_connectionString);
+        }
+    }
+}
